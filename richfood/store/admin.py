208c3e14bd4263885422f53f4102b04883225763
Mohammad Belal Hossain <62785546+belalahmed143@users.noreply.github.com>
2023-08-03 20:11:03 +0600

    class18

--- richfood/store/admin.py
+++ richfood/store/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+my_models = [Banner,Category,Brand,Product,ProductRelatedImage]
+
+admin.site.register(my_models)
--- richfood/store/views.py
+++ richfood/store/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
-    return render(request, 'store/index.html', {})
+    banners = Banner.objects.all()[:2]
+    banners1 = Banner.objects.all()[2:11]
+
+
+    context ={
+        'banners':banners,
+        'banners1':banners1
+    }
+    return render(request, 'store/index.html',context)
     
